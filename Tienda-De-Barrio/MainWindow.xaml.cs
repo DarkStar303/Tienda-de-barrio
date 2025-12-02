8853a7bf24e002c9d9dfd70d5f96b9b2abef9831
goemon043 <goemon043@gmail.com>
2025-12-02 19:33:22 -0400

    Ventanas con estilos (3)

---
+++ Tienda-De-Barrio/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tienda_De_Barrio
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSiguiente_Click(object sender, RoutedEventArgs e)
+        {
+            Administracion ad=new Administracion();
+            ad.Show();
+            this.Close();
+
+        }
+    }
+}
