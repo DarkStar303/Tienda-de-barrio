2a3a92b8808fe7a61c5a5e867085c60c4257fdd9
goemon043 <goemon043@gmail.com>
2025-12-03 11:09:09 -0400

    se agrego nuevas ventanas (unas 2)

---
+++ Administracion.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tienda_De_Barrio
+{
+    /// <summary>
+    /// Lógica de interacción para Administracion.xaml
+    /// </summary>
+    public partial class Administracion : Window
+    {
+        public Administracion()
+        {
+            InitializeComponent();
+        }
+
+        private void Vender_CLICK(object sender, RoutedEventArgs e)
+        {
+            Venta ven= new Venta();
+            ven.Show();
+            this.Close();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
